90c3698e13d7313036e09a2958ae521bde5cf5e5
Julia <julia.poo.poo.poo@gmail.com>
2021-02-20 23:40:06 +0800

    Very Minor Changes cuz it looks _slightly_ nicer
    Signed-off-by: Julia <julia.poo.poo.poo@gmail.com>

--- Whitehacks2020/Concatacollision/solve.py
+++ Whitehacks2020/Concatacollision/solve.py
@@ -6,9 +6,9 @@ check = lambda s: int(str(int(str(x1) + str(s)) % x0) + str(s)) % x1
 
 p = len(str(x0*x1))
 s = pow(x0,-1,x1)*x0 - pow(x1,-1,x0)*x1*x1*10**p
-s = s % (x0*x1)
+s %= (x0*x1)
 
-assert check(s)==1, len(s) == p-1
+assert check(s)==1, len(s)==p-1
 print(s)
 
 # > 31175392867133471711106087115764756200474617904802451549159453225242446295040
